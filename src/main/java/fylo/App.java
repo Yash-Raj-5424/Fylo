31c898975fef6cbc8c4731255a6aece487cb9210
Yash-Raj-5424 <myash3499@gmail.com>
2026-06-10 00:44:00 +0530

    feat: add server logic - initialization, upload, shutdown

--- src/main/java/fylo/App.java
+++ src/main/java/fylo/App.java
@@ -1,8 +1,30 @@
 package fylo;
 
 
+import fylo.controller.FileController;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        try{
+            FileController fileController = new FileController(8080);
+            fileController.start();
+
+            System.out.println("Server started on port 8080. Press Ctrl+C to stop");
+            System.out.println("UI is available at http://localhost:3000");
+
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                System.out.println("Shutting down the server...");
+                fileController.stop();
+            }));
+
+
+
+
+        }catch (Exception e){
+            System.err.println("Failed to start the server: " + e.getMessage());
+            e.printStackTrace();
+        }
+
     }
 }
